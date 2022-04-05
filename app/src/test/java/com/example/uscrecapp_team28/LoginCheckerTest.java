19735bde7c2c37f9098f10949980231ea55dbf4d
Tigojiang <tigojian@usc.edu>
2022-04-04 22:20:09 -0700

    Update LoginCheckerTest.java

--- app/src/test/java/com/example/uscrecapp_team28/LoginCheckerTest.java
+++ app/src/test/java/com/example/uscrecapp_team28/LoginCheckerTest.java
@@ -2,81 +2,31 @@ package com.example.uscrecapp_team28;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
+
 public class LoginCheckerTest {
-    @Mock
-    Connection conn;
-
-    @Mock
-    PreparedStatement pst;
-
-    @Mock
-    ResultSet result;
-
-    @Captor
-    ArgumentCaptor<String> stringCaptor;
-
-    AutoCloseable closeable;
 
     @Before
     public void setup() {
-        closeable = MockitoAnnotations.openMocks(this);
+
+
     }
 
     @After
     public void cleanup() throws Exception {
-        closeable.close();
     }
 
     @Test
-    public void check_login() {
+    public void test_login_success(){
+        LoginChecker loginChecker = new LoginChecker("test","test","deviceid2");
+        loginChecker.check_login();
+        assertTrue(loginChecker.getLoginFlag());
     }
-
-    @Test
-    public void getUsername() {
-    }
-
-    @Test
-    public void setUsername() {
-    }
-
-    @Test
-    public void getPassword() {
-    }
-
-    @Test
-    public void setPassword() {
-    }
-
-    @Test
-    public void getLoginFlag() {
-    }
-
-    @Test
-    public void setLoginFlag() {
-    }
-
 }
