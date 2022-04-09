5b8538b11160da07df59535afd7bd9c77086f1ca
Tigojiang <tigojian@usc.edu>
2022-04-08 22:33:20 -0700

    login intent test

--- app/src/androidTest/java/com/example/uscrecapp_team28/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/uscrecapp_team28/LoginActivityTest.java
@@ -1,16 +1,31 @@
 package com.example.uscrecapp_team28;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
+import android.content.Intent;
+
+import androidx.test.espresso.core.internal.deps.guava.collect.Iterables;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
+import static androidx.test.ext.truth.content.IntentSubject.assertThat;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -19,9 +34,16 @@ public class LoginActivityTest {
     @Rule
     public ActivityTestRule<LoginActivity> activityRule =
             new ActivityTestRule<>(LoginActivity.class);
-
+    @Rule
+    public IntentsTestRule<MapActivity> intentsTestRule =
+            new IntentsTestRule<>(MapActivity.class);
     @Test
-    public void onLoginHelper() {
+    public void onLoginSuccess() {
+        onView(withId(R.id.username)).perform(typeText("test_login"));
+        onView(withId(R.id.password)).perform(typeText("test_login"));
+        onView(withId(R.id.signinbtn)).perform(click());
+        intended(hasComponent("com.example.uscrecapp_team28.MapActivity"));
+
     }
 
     @Test
