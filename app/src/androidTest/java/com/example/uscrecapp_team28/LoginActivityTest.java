1581f6c4f5f76b7587c4802254e9deebc9c6f79b
Dongming Shen <alvinshe@usc.edu>
2022-04-08 22:08:16 -0700

    login bb test 1

--- app/src/androidTest/java/com/example/uscrecapp_team28/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/uscrecapp_team28/LoginActivityTest.java
@@ -1,7 +1,13 @@
 package com.example.uscrecapp_team28;
 
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.action.ViewActions.*;
 import static org.junit.Assert.*;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
@@ -12,6 +18,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class LoginActivityTest {
@@ -21,7 +29,19 @@ public class LoginActivityTest {
             new ActivityTestRule<>(LoginActivity.class);
 
     @Test
-    public void onLoginHelper() {
+    public void onLoginSuccess() {
+        onView(withId(R.id.username)).perform(typeText("test_login"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("test_login"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.signinbtn)).perform(click());
+        onView(withId(R.id.wrong)).check(doesNotExist());
+    }
+
+    @Test
+    public void onLoginFail() {
+        onView(withId(R.id.username)).perform(typeText("test_login"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("test_login_wrong"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.signinbtn)).perform(click());
+        onView(withId(R.id.wrong)).check(matches(withText("Wrong username/password. Try again.")));
     }
 
     @Test
